d7cd23f7a420f5d4de854df25dcf4098e0d4552d
jessestephenbrown <jessestephenbrown@gmail.com>
2015-02-24 16:40:37 -0700

    Update bootstrap for gensim (and dependencies)

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,9 @@
 apt-get update
 apt-get install -y python-pip
 apt-get install -y python-numpy
+apt-get install -y python-scipy
 apt-get install -y python-matplotlib
 apt-get install -y python-dev
 pip install lda
 pip install nltk
+easy_install --upgrade gensim
